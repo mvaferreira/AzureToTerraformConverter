5b224e1d7e4c92943e137f9aabd412a818feccb4
Marcus Ferreira <marcus.ferreira@microsoft.com>
2020-12-09 17:16:16 -0300

    added resources.

--- Convert_Az2TF.ps1
+++ Convert_Az2TF.ps1
@@ -19,18 +19,26 @@ Disclaimer
 	    This script will read Azure resources via "Azure CLI" and convert resources to Terraform files.
     
     .EXAMPLE
-        Run script with -SubscriptionID parameter.
+        Run script with -SubscriptionsID parameter.
 
-        .\Convert_Az2TF.ps1 -SubscriptionID "<my_subscription_ID>"
+        .\Convert_Az2TF.ps1 -SubscriptionsID @("<my_subscription_ID1>","<my_subscription_ID2>")
 #>
 
 Param(
     [Parameter(Mandatory = $True)]
-    $SubscriptionID = ""
+    $SubscriptionsID = @()
 )
 
-Write-Host "[$(Get-Date)] Finding resources in subscription $($SubscriptionID)..."
-$Resources = az graph query -q  "resources | where subscriptionId == '$SubscriptionID' | order by id asc" --output json --only-show-errors | ConvertFrom-Json
+$Resources = @()
+$ResourceGroups = @()
+
+ForEach($SubscriptionID In $SubscriptionsID) {
+    Write-Host "[$(Get-Date)] Finding resources in subscription $($SubscriptionID)..."
+    $Resources += az graph query -q  "resources | where subscriptionId == '$SubscriptionID' | order by id asc" --output json --only-show-errors | ConvertFrom-Json
+    $ResourceGroups += az graph query -q  "resourceContainers | where subscriptionId == '$SubscriptionID' | order by id asc" --output json --only-show-errors | ConvertFrom-Json
+}
+
+$DiskResources = $Resources | Where-Object {$_.Type -eq 'microsoft.compute/disks'}
 
 $TFDirectory = "terraform"
 $TFMainFile = "main.tf"
@@ -40,6 +48,7 @@ $AllNICs = @{ }
 $AllSTGs = @{ }
 $AllVMs = @{ }
 $AllVNets = @{ }
+$AllDisks = @{ }
 $Global:TFResources = @()
 $Global:TFImport = @()
 $NL = "`r`n"
@@ -56,8 +65,11 @@ Function GetTFAzProvider() {
 
 Function GetTFResourceGroup($Obj) {
     $Global:TFResources += "resource `"azurerm_resource_group`" `"rg_$($Obj.Name)`" {" + $NL +
-    "$($SP2)name     = `"$($Obj.Name)`"" + $NL +
-    "$($SP2)location = `"$($Obj.Location)`"" + $NL +
+    "$($SP2)name     = `"$($Obj.Name)`""
+
+    $RG = $ResourceGroups | Where-Object {$_.id -eq $Obj.Id}
+
+    $Global:TFResources += "$($SP2)location = `"$($RG.location)`"" + $NL +
     "}" + $NL
 
     $Global:TFImport += "terraform import azurerm_resource_group.rg_$($Obj.Name) $($Obj.Id)"  
@@ -125,6 +137,34 @@ Function GetTFNetInterface($Obj) {
     $Global:TFImport += "terraform import azurerm_network_interface.nic_$($Obj.Name) $($Obj.Id)"
 }
 
+Function GetTFManagedDisk($Obj) {
+    $Global:TFResources += "resource `"azurerm_managed_disk`" `"disk_$($Obj.Name)`" {" + $NL +
+    "$($SP2)name                 = `"$($Obj.Name)`"" + $NL +
+    "$($SP2)location             = `"$($Obj.Location)`"" + $NL +
+    "$($SP2)resource_group_name  = azurerm_resource_group.rg_$($Obj.ResourceGroup.Name).name" + $NL +
+    "$($SP2)storage_account_type = `"$($Obj.StorageAccountType)`"" + $NL +
+    "$($SP2)create_option        = `"$($Obj.CreateOption)`"" + $NL +
+    "$($SP2)disk_size_gb         = `"$($Obj.SizeGB)`""
+
+    $Global:TFResources += "}" + $NL
+
+    $Global:TFImport += "terraform import azurerm_managed_disk.disk_$($Obj.Name) $($Obj.Id)"
+}
+
+Function GetTFDataDiskAttachment($Obj) {
+    $VM = $Resources | Where-Object {$_.id -eq $Obj.VMId}
+    $VMDataDisks = $VM.properties.storageprofile.dataDisks
+
+    $Global:TFResources += "resource `"azurerm_virtual_machine_data_disk_attachment`" `"disk_att_$($Obj.Name)`" {" + $NL +
+    "$($SP2)managed_disk_id    = azurerm_managed_disk.disk_$($Obj.Name).id" + $NL +
+    "$($SP2)virtual_machine_id = azurerm_windows_virtual_machine.vm_$($VM.name).id" + $NL +
+    "$($SP2)lun                = $($VMDataDisks.lun)" + $NL +
+    "$($SP2)caching            = `"$($VMDataDisks.caching)`"" + $NL +
+    "}" + $NL
+
+    $Global:TFImport += "terraform import azurerm_virtual_machine_data_disk_attachment.disk_att_$($Obj.Name) $($Obj.Id)"    
+}
+
 Function GetTFWindowsVM($Obj) {
     $Nics = $Obj.Nics
 
@@ -133,18 +173,33 @@ Function GetTFWindowsVM($Obj) {
     "$($SP2)resource_group_name   = azurerm_resource_group.rg_$($Obj.ResourceGroup.Name).name" + $NL +
     "$($SP2)location              = `"$($Obj.Location)`"" + $NL +
     "$($SP2)size                  = `"$($Obj.Size)`"" + $NL +
-    "$($SP2)admin_username        = `"$($Obj.Name)`"" + $NL +
+    "$($SP2)admin_username        = `"$($Obj.AdminUsername)`"" + $NL +
     "$($SP2)admin_password        = `"$($Obj.Name)`"" + $NL +
     "$($SP2)network_interface_ids = ["
     
     ForEach ($Nic In $Nics) {
         $Split = $Nic.NicId -split '/'
         $NicName = $Split[$Split.count - 1]
-        $Global:TFResources += "azurerm_network_interface.nic_$($NicName).id" -join ','
+        $Global:TFResources += "$($SP2)$($SP2)azurerm_network_interface.nic_$($NicName).id" -join ','
     }
     
-    $Global:TFResources += "]" + $NL +
-    "}" + $NL
+    $Global:TFResources += "$($SP2)]" + $NL
+
+    $OSDisk = $Obj.Disks | Where-Object {$_.OsType}
+
+    $Global:TFResources += "$($SP2)os_disk {" + $NL +
+    "$($SP2)$($SP2)caching              = `"ReadWrite`"" + $NL +
+    "$($SP2)$($SP2)storage_account_type = `"$($OSDisk.StorageAccountType)`"" + $NL +
+    "$($SP2)}" + $NL
+
+    $Global:TFResources += "$($SP2)source_image_reference {" + $NL +
+    "$($SP2)$($SP2)publisher = `"$($Obj.Publisher)`"" + $NL +
+    "$($SP2)$($SP2)offer     = `"$($Obj.Offer)`"" + $NL +
+    "$($SP2)$($SP2)sku       = `"$($Obj.Sku)`"" + $NL +
+    "$($SP2)$($SP2)version   = `"$($Obj.Version)`"" + $NL +
+    "$($SP2)}"
+
+    $Global:TFResources += "}" + $NL
 
     $Global:TFImport += "terraform import azurerm_windows_virtual_machine.vm_$($Obj.Name) $($Obj.Id)"
 }
@@ -157,13 +212,12 @@ ForEach ($Resource In $Resources) {
             $Properties = $Resource.Properties
 
             $Obj = [PSCustomObject]@{
-                Id                     = $Resource.Id
-                Name                   = $Resource.Name
-                Location               = $Resource.location
-                ResourceGroup          = [PSCustomObject]@{
-                    Id       = "/subscriptions/$($Resource.subscriptionId)/resourceGroups/$($Resource.resourceGroup)"
-                    Name     = $Resource.resourceGroup
-                    Location = $Resource.location
+                Id            = $Resource.Id
+                Name          = $Resource.Name
+                Location      = $Resource.location
+                ResourceGroup = [PSCustomObject]@{
+                    Id        = "/subscriptions/$($Resource.subscriptionId)/resourceGroups/$($Resource.resourceGroup)"
+                    Name      = $Resource.resourceGroup
                 }
                 AccountTier            = ($Resource.sku.name -split '_')[0]
                 AccountReplicationType = ($Resource.sku.name -split '_')[1]     
@@ -184,9 +238,8 @@ ForEach ($Resource In $Resources) {
                 Name          = $Resource.Name
                 Location      = $Resource.location
                 ResourceGroup = [PSCustomObject]@{
-                    Id       = "/subscriptions/$($Resource.subscriptionId)/resourceGroups/$($Resource.resourceGroup)"
-                    Name     = $Resource.resourceGroup
-                    Location = $Resource.location
+                    Id        = "/subscriptions/$($Resource.subscriptionId)/resourceGroups/$($Resource.resourceGroup)"
+                    Name      = $Resource.resourceGroup
                 }
                 AddressSpaces = @()
                 Subnets       = @()
@@ -203,13 +256,12 @@ ForEach ($Resource In $Resources) {
                 $SubnetProperties = $Subnet.Properties
 
                 $Obj.Subnets += [PSCustomObject]@{
-                    Id                 = $Subnet.id
-                    Name               = $Subnet.name
-                    AddressPrefix      = $SubnetProperties.addressPrefix
-                    ResourceGroup      = [PSCustomObject]@{
-                        Id       = "/subscriptions/$($Resource.subscriptionId)/resourceGroups/$($Resource.resourceGroup)"
-                        Name     = $Resource.resourceGroup
-                        Location = $Resource.location
+                    Id            = $Subnet.id
+                    Name          = $Subnet.name
+                    AddressPrefix = $SubnetProperties.addressPrefix
+                    ResourceGroup = [PSCustomObject]@{
+                        Id        = "/subscriptions/$($Resource.subscriptionId)/resourceGroups/$($Resource.resourceGroup)"
+                        Name      = $Resource.resourceGroup
                     }
                     VirtualNetworkName = $Resource.Name                 
                 }
@@ -228,9 +280,8 @@ ForEach ($Resource In $Resources) {
                 Name          = $Resource.Name
                 Location      = $Resource.location
                 ResourceGroup = [PSCustomObject]@{
-                    Id       = "/subscriptions/$($Resource.subscriptionId)/resourceGroups/$($Resource.resourceGroup)"
-                    Name     = $Resource.resourceGroup
-                    Location = $Resource.location
+                    Id        = "/subscriptions/$($Resource.subscriptionId)/resourceGroups/$($Resource.resourceGroup)"
+                    Name      = $Resource.resourceGroup
                 }
                 IPConfigs     = @()
             }
@@ -255,18 +306,24 @@ ForEach ($Resource In $Resources) {
         'microsoft.compute/virtualmachines' {
             $Properties = $Resource.Properties
             $Nics = $Properties.networkProfile.networkinterfaces
+            $ImageReference = $Properties.storageProfile.imagereference
 
             $Obj = [PSCustomObject]@{
                 Id            = $Resource.Id
                 Name          = $Resource.Name
                 Location      = $Resource.location
                 ResourceGroup = [PSCustomObject]@{
-                    Id       = "/subscriptions/$($Resource.subscriptionId)/resourceGroups/$($Resource.resourceGroup)"
-                    Name     = $Resource.resourceGroup
-                    Location = $Resource.location
+                    Id        = "/subscriptions/$($Resource.subscriptionId)/resourceGroups/$($Resource.resourceGroup)"
+                    Name      = $Resource.resourceGroup
                 }
                 Size          = $Properties.hardwareProfile.vmSize
                 Nics          = @()
+                Disks         = @()
+                AdminUsername = $Properties.osProfile.adminUsername
+                Publisher     = $ImageReference.publisher
+                Offer         = $ImageReference.offer
+                Sku           = $ImageReference.sku
+                Version       = $ImageReference.version
             }
 
             ForEach ($Nic In $Nics) {
@@ -274,9 +331,59 @@ ForEach ($Resource In $Resources) {
                     NicId = $Nic.id
                 }                
             }
-        
+
+            $VMDisks = $DiskResources | Where-Object {$_.managedBy -eq $Resource.Id}
+
+            ForEach($VMDisk In $VMDisks) {
+                $Obj.Disks += [PSCustomObject]@{
+                    Id                 = $VMDisk.id
+                    Name               = $VMDisk.name
+                    Location           = $VMDisk.location
+                    VMId               = $VMDisk.managedBy
+                    SizeGB             = $VMDisk.properties.diskSizeGB
+                    State              = $VMDisk.properties.diskState
+                    OsType             = $VMDisk.properties.osType
+                    StorageAccountType = $VMDisk.sku.name
+                    CreateOption       = "Empty"
+                    ResourceGroup      = [PSCustomObject]@{
+                        Id             = "/subscriptions/$($Resource.subscriptionId)/resourceGroups/$($Resource.resourceGroup)"
+                        Name           = $Resource.resourceGroup
+                    }                    
+                }
+            }
+
+            $DataDisks = $Obj.Disks | Where-Object {-Not $_.OsType}
+
+            ForEach($DataDisk In $DataDisks) {
+                If (-Not $AllDisks.Contains($DataDisk.Id)) { $AllDisks.Add($DataDisk.Id, $DataDisk) }
+            }
+            
             If (-Not $AllRGs.Contains($Obj.ResourceGroup.Id)) { $AllRGs.Add($Obj.ResourceGroup.Id, $Obj.ResourceGroup) }
             If (-Not $AllVMs.Contains($Obj.Id)) { $AllVMs.Add($Obj.Id, $Obj) }
+        }
+
+        'microsoft.compute/disks' {
+            #We only care for data disks
+            If (-Not $Resource.properties.osType) {
+                $Obj = [PSCustomObject]@{
+                    Id            = $Resource.Id
+                    Name          = $Resource.Name
+                    Location      = $Resource.location
+                    ResourceGroup = [PSCustomObject]@{
+                        Id        = "/subscriptions/$($Resource.subscriptionId)/resourceGroups/$($Resource.resourceGroup)"
+                        Name      = $Resource.resourceGroup
+                    }
+                    VMId               = $Resource.managedBy
+                    SizeGB             = $Resource.properties.diskSizeGB
+                    State              = $Resource.properties.diskState
+                    OsType             = $Resource.properties.osType
+                    StorageAccountType = $Resource.sku.name
+                    CreateOption       = "Empty"                                
+                }          
+
+                If (-Not $AllRGs.Contains($Obj.ResourceGroup.Id)) { $AllRGs.Add($Obj.ResourceGroup.Id, $Obj.ResourceGroup) }
+                If (-Not $AllDisks.Contains($Obj.Id)) { $AllDisks.Add($Obj.Id, $Obj) }
+            }
         }
     }
 
@@ -295,6 +402,8 @@ $AllSTGs.Values | ForEach-Object { GetTFStorageAccount($_) }
 $AllVNets.Values | ForEach-Object { GetTFVirtualNetworks($_) }
 $AllVNets.Values.Subnets | ForEach-Object { GetTFSubnets($_) }
 $AllNICs.Values | ForEach-Object { GetTFNetInterface($_) }
+$AllDisks.Values | ForEach-Object { GetTFManagedDisk($_) }
+$AllDisks.Values | Where-Object {$_.VMId} | ForEach-Object { GetTFDataDiskAttachment($_) }
 $AllVMs.Values | ForEach-Object { GetTFWindowsVM($_) }
 
 $null = New-Item -Name $TFResourcesFile -Path (Join-Path (Get-Location).Path $TFDirectory) -Type File -Value ($TFResources -Join $NL) -Force
